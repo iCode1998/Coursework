95d2f71ad969b919b86534ef5d870d1ca08a4668
Michael Donnarumma <mike@Michaels-MacBook-Pro.local>
2021-11-04 23:00:13 +0000

    Version 2

---
+++ src/lessons.js
@@ -0,0 +1,89 @@
+const maths = {
+    "id":"1",
+    "title":"Maths",
+    "locations": "London",
+    "price":100,
+    "image":"assets/maths.png",
+    "spaces":5,
+} 
+
+const history = {
+    "id":"2",
+    "title":"History",
+    "locations": "Turin",
+    "price":150,
+    "image":"assets/history-book.png",
+    "spaces":5,
+} 
+
+const science= {
+    "id":"3",
+    "title":"Science",
+    "locations":"Rome",
+    "price":120,
+    "image":"assets/science.png",
+    "spaces":5,
+} 
+
+const geography = {
+    "id":"4",
+    "title":"Geography",
+    "locations": "Paris",
+    "price":130,
+    "image":"assets/globe.png",
+    "spaces":5,
+} 
+
+const english = {
+    "id":"5",
+    "title":"English",
+    "locations": "Instanbul",
+    "price":100,
+    "image":"assets/uk.png",
+    "spaces":5,
+} 
+
+const marketing= {
+    "id":"6",
+    "title":"Marketing",
+    "locations": "Budapest",
+    "price":150,
+    "image":"assets/social-marketing.png",
+    "spaces":5,
+} 
+
+const computer_science = {
+    "id":"7",
+    "title":"Computer Science",
+    "locations": "London",
+    "price":200,
+    "image":"assets/cs.png",
+    "spaces":5,
+} 
+
+const biology = {
+    "id":"8",
+    "title":"Biology",
+    "locations": "Berlin",
+    "price":170,
+    "image":"assets/biology.png",
+    "spaces":5,
+} 
+
+const chemistry = {
+    "id":"9",
+    "title":"Maths",
+    "locations": "London",
+    "price":100,
+    "image":"assets/pt.png",
+    "spaces":5,
+} 
+
+const art = {
+    "id":"10",
+    "title":"Art",
+    "locations": "Florence",
+    "price":140,
+    "image":"assets/palette.png",
+    "spaces":5,
+} 
--- src/vue.js
+++ src/vue.js
@@ -2,28 +2,17 @@ var vueApp = new Vue({
     el: '#app',
     data: {
         sitename: 'After School Club',
+        lessons: [maths,history,science,geography,english,marketing,computer_science,biology,chemistry,art],
         cart:[],
-        // Creates item
-        product: {
-            scope: this,
-            id: 3377,
-            title: 'Maths',
-            location: 'London',
-            price: 'Price: £100',
-            image: "assets/math.png",
-            btn: 'Add To Cart',
-            spaces: 5,
-
-            // Adds item to cart array and decreases spaces
-            addToCart: function(){
-                const cart = this.scope.vueApp.cart;
-                cart.push(this.id);
-                this.spaces--;
-            },
-        }
     },
+
     methods:{
-        AddToCart: function(){ this.product.addToCart(); }
+
+        addToCart: function(){
+            const cart = this.scope.vueApp.cart;
+            cart.push(this.id);
+            this.spaces--;
+        },
     },
 
     computed:{
